0d9ce6bcaa1adf6fd63662c7cd0ec3d90f8816d2
Oleg <oleghools@gmail.com>
2023-10-21 19:50:57 +0200

    a

--- gpt_toolkit.py
+++ gpt_toolkit.py
@@ -1,17 +1,18 @@
+import json
 import openai
 
 
 def autocomplete(prompt: str) -> str:
     try:
         completion = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
             max_tokens=1000,
             temperature=0.8,
         )
     except:
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
             messages=[{"role": "user", "content": prompt}],
             max_tokens=1000,
             temperature=0.8,
@@ -35,7 +36,7 @@ Headline: {profile['headline']}
 Description: {profile['description']}
 Work experience from the latest to the earliest:
 {work_experience}
-Write a short summary of this person's career path (ONLY 7 SENTENCES MAXIMUM), include notable and unusual facts about him/her. Mention details on hir/her development studio:
+Write a short summary of this person's career path (ONLY 7 SENTENCES MAXIMUM), include notable and unusual facts about him/her. Mention details on his/her development studio:
 """
     print(prompt, "\n")
     return autocomplete(prompt)
@@ -43,20 +44,42 @@ Write a short summary of this person's career path (ONLY 7 SENTENCES MAXIMUM), i
 
 def get_marketing_letter(personal_summmary: str) -> str:
     prompt = f"""You're a professional marketing and cold b2b outreach expert. Your task is to write an email letter promoting b2b lead generation service for software development company.
-    Requirements:
-    - write a subject line (max 50 characters) that stands out and makes the recipient want to open the email. use small letters to stand out. use personalization
+    REQUIREMENTS:
+    - write a personalised subject line (max 50 characters) that stands out and makes the recipient want to open the email. use small letters to stand out. assume familiarity with the recipient
     - write a short email body (max 100 words) that makes the recipient want to reply
     - use AIDA framework (Attention, Interest, Desire, Action) to structure your email
-    - in the beginning of email give a compliment to the recipient
-    - in the end if email say that if the recipient is interested in your offer, we can schedule a call
+    - in the beginning of email give a small compliment to recipient, don't be lame
+    - in the end if email say that if the recipient is interested in our offer, we can schedule a call
     - in the end write 'Kind regards, Oleg Melnikov | www.evolva.ai'
+    - don't mention evolva ai in the email body
     What exact services that you're selling:
     - booking meetings with qualified potential clients for software development companies
     - we use cold email for it, but we do it in a very personalized way
     - we work on performance basis, so we charge only for actual meetings with qualified leads
+    - we guarantee at the vert least 5 meetings with qualified leads per month, otherwise you don't pay
     - there's no work on the client's side, we do everything ourselves
     Summary of the person that we're reaching out to and his/her company:
     {personal_summmary}
-    Write an email letter according to the requirements above:"""
+    Write an email letter according to the REQUIREMENTS above, also add TWO follow-up letters WITHOUT a subject line, address a pain point there and mention a call again:"""
     print(prompt, "\n")
     return autocomplete(prompt)
+
+
+def get_json_packaged_marketing_letters(letters: str) -> dict:
+    prompt = f"""Take given text and package it into a JSON file in the following format:
+{{
+    "subject": "Subject line of the email",
+    "body": "Hi Name, 
+    ....
+    ...",
+    "follow_up_1": "First follow-up email
+    ....",
+    "follow_up_2": "Second follow-up email
+    ..."
+}}
+Given text:
+{letters}
+JSON file:
+"""
+    print(prompt, "\n")
+    return json.loads(autocomplete(prompt))
--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import json
 from settings import Settings
 import openai
 from linkedin_parsing import parse_profile
-from gpt_toolkit import get_linkedin_profile_summary, get_marketing_letter
+from gpt_toolkit import *
+import pandas as pd
 
 
 def main():
-    linkedin_url = "https://www.linkedin.com/in/yonas-khorsi/"  # https://www.linkedin.com/in/yonas-khorsi/ https://www.linkedin.com/in/pavel-fakanov/ https://www.linkedin.com/in/husnain-pasha-66158119b/ https://www.linkedin.com/in/fokinadaria/ https://www.linkedin.com/in/fritz-wierper-6933b6104/
+    linkedin_url = "https://www.linkedin.com/in/ivan-pavlovich-363216219/"  # https://www.linkedin.com/in/ivan-pavlovich-363216219/ https://www.linkedin.com/in/yonas-khorsi/ https://www.linkedin.com/in/pavel-fakanov/ https://www.linkedin.com/in/husnain-pasha-66158119b/ https://www.linkedin.com/in/fokinadaria/ https://www.linkedin.com/in/fritz-wierper-6933b6104/
     settings = Settings()
     openai.api_key = settings.OPENAI_API_TOKEN
     parsed_profile = parse_profile(linkedin_url, settings)
     print(json.dumps(parsed_profile, indent=4), "\n")
     summary = get_linkedin_profile_summary(parsed_profile)
     print(summary, "\n")
-    marketing_letter = get_marketing_letter(summary)
-    print(marketing_letter, "\n")
+    marketing_letters = get_marketing_letter(summary)
+    print(marketing_letters, "\n")
+    json_packaged_marketing_letters = get_json_packaged_marketing_letters(
+        marketing_letters
+    )
+    print(json_packaged_marketing_letters, "\n")
+    df = pd.DataFrame([json_packaged_marketing_letters])
+    df.to_csv("result.csv")
 
 
 if __name__ == "__main__":
